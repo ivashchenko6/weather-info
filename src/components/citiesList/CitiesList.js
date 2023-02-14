8c1d53265c02e20ee73b557de6f00f623a807054
Kirill <kirill040605@gmail.com>
2023-02-13 22:48:58 -0800

    new message for unknown cities

--- src/components/citiesList/CitiesList.js
+++ src/components/citiesList/CitiesList.js
@@ -5,7 +5,8 @@ import './citiesList.scss';
 const CitiesList = ({data}) => {
 
     
-    const items = data.map((item, i) => <WeatherCard key={i} item={item} />)
+    const items = data.length ? data.map((item, i) => <WeatherCard key={i} item={item} />) : <h1>We don`t have this city</h1>
+    
     return (
         <main className="main-section">
             <ul className="cities-list">
